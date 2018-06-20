62d8fa525036634e2ea2ef3abb7db51a1bf0777c
ftorto <dev.torto@gmail.com>
2018-06-20 11:46:44 +0200

    transparent proxy ports can be customized
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- assets/init.sh
+++ assets/init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Setup ports
+HTTP_RELAY_PORT=22345
+HTTP_CONNECT_PORT=22346
+SOCKS5_PORT=22347
+
 iptables_rules_docker() {
 
     # private ip ranges are not intercepted
@@ -10,9 +15,9 @@ iptables_rules_docker() {
         iptables -t nat -$1 PREROUTING -i docker0 -d ${no_proxy_url} -j RETURN 2>/dev/null
     done
 
-    iptables -t nat -$1 PREROUTING -p tcp --dport 80   -i docker0 -j REDIRECT --to 12345 2>/dev/null
-    iptables -t nat -$1 PREROUTING -p tcp --dport 8080 -i docker0 -j REDIRECT --to 12345 2>/dev/null
-    iptables -t nat -$1 PREROUTING -p tcp --dport 443  -i docker0 -j REDIRECT --to 12346 2>/dev/null
+    iptables -t nat -$1 PREROUTING -p tcp --dport 80   -i docker0 -j REDIRECT --to ${HTTP_RELAY_PORT} 2>/dev/null
+    iptables -t nat -$1 PREROUTING -p tcp --dport 8080 -i docker0 -j REDIRECT --to ${HTTP_RELAY_PORT} 2>/dev/null
+    iptables -t nat -$1 PREROUTING -p tcp --dport 443  -i docker0 -j REDIRECT --to ${HTTP_CONNECT_PORT} 2>/dev/null
 }
 
 
@@ -27,17 +32,17 @@ iptables_rules_other() {
         iptables -t nat -${MODE} OUTPUT -d ${no_proxy_url} -p tcp  -j RETURN 2>/dev/null
     done
 
-    iptables -t nat -${MODE} PREROUTING -p tcp ! -i docker0 --dport 80   -j REDIRECT --to 12345 2>/dev/null
-    iptables -t nat -${MODE} PREROUTING -p tcp ! -i docker0 --dport 8080 -j REDIRECT --to 12345 2>/dev/null
-    iptables -t nat -${MODE} PREROUTING -p tcp ! -i docker0 --dport 443  -j REDIRECT --to 12346 2>/dev/null 
+    iptables -t nat -${MODE} PREROUTING -p tcp ! -i docker0 --dport 80   -j REDIRECT --to ${HTTP_RELAY_PORT} 2>/dev/null
+    iptables -t nat -${MODE} PREROUTING -p tcp ! -i docker0 --dport 8080 -j REDIRECT --to ${HTTP_RELAY_PORT} 2>/dev/null
+    iptables -t nat -${MODE} PREROUTING -p tcp ! -i docker0 --dport 443  -j REDIRECT --to ${HTTP_CONNECT_PORT} 2>/dev/null 
 
     iptables -t nat -${MODE} OUTPUT -d $(parse_ip $http_proxy) -p tcp  -j RETURN 2>/dev/null
     # HTTP and HTTPS
-    iptables -t nat -${MODE} OUTPUT -p tcp --dport 80 -j REDIRECT --to-ports 12345 2>/dev/null
-    iptables -t nat -${MODE} OUTPUT -p tcp --dport 443 -j REDIRECT --to-ports 12346 2>/dev/null
+    iptables -t nat -${MODE} OUTPUT -p tcp --dport 80 -j REDIRECT --to-ports ${HTTP_RELAY_PORT} 2>/dev/null
+    iptables -t nat -${MODE} OUTPUT -p tcp --dport 443 -j REDIRECT --to-ports ${HTTP_CONNECT_PORT} 2>/dev/null
     # Any other port 
-    iptables -t nat -${MODE} OUTPUT -p tcp --dport 22 -j REDIRECT --to-ports 12347 2>/dev/null
-    iptables -t nat -${MODE} OUTPUT -p tcp -j REDIRECT --to-ports 12346 2>/dev/null
+    iptables -t nat -${MODE} OUTPUT -p tcp --dport 22 -j REDIRECT --to-ports ${SOCKS5_PORT} 2>/dev/null
+    iptables -t nat -${MODE} OUTPUT -p tcp -j REDIRECT --to-ports ${HTTP_CONNECT_PORT} 2>/dev/null
 
 }
 
@@ -99,7 +104,7 @@ run() {
 
     ip=$(parse_ip $http_proxy)
     port=$(parse_port $http_proxy)
-    append_redsocks_conf "http-relay" $ip $port "12345"
+    append_redsocks_conf "http-relay" $ip $port "${HTTP_RELAY_PORT}"
 
     if [ -z "$https_proxy" ]; then
         https_proxy="$http_proxy"
@@ -107,10 +112,16 @@ run() {
 
     ip=$(parse_ip $https_proxy)
     port=$(parse_port $https_proxy)
-    append_redsocks_conf "http-connect" $ip $port "12346"
-    append_redsocks_conf "socks5" $ip $port "12347"
+    append_redsocks_conf "http-connect" $ip $port "${HTTP_CONNECT_PORT}"
+    append_redsocks_conf "socks5" $ip $port "${SOCKS5_PORT}"
 
     iptables_rules A
+
+    echo "Ports setup :"
+    echo "HTTP_RELAY_PORT=${HTTP_RELAY_PORT}"
+    echo "HTTP_CONNECT_PORT=${HTTP_CONNECT_PORT}"
+    echo "SOCKS5_PORT=${SOCKS5_PORT}"
+    
     redsocks -c /app/redsocks.conf
 }
 
