39a1fe249df86d55b4551c5f66529ff960f4f0f6
ftorto <dev.torto@gmail.com>
2018-06-20 17:54:44 +0200

    easily switch docker proxy configuration
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- docker_conf_proxy.py
+++ docker_conf_proxy.py
@@ -2,16 +2,21 @@
 
 import sys
 import json
+import os
 
 from os.path import expanduser
 home = expanduser("~")
 
+HTTP_PROXY = os.environ.get('HTTP_PROXY', "http://127.0.0.1:3128")
+HTTPS_PROXY = os.environ.get('HTTPS_PROXY', HTTP_PROXY)
+
 PROXY_CONFIG = {
     "default": {
-        "httpProxy": "http://127.0.0.1:3128",
-        "httpsProxy": "http://127.0.0.1:3128",
+        "httpProxy": HTTP_PROXY,
+        "httpsProxy": HTTPS_PROXY,
     }
 }
+
 
 def activate():
     docker_config = {}
@@ -21,6 +26,7 @@ def activate():
     with open("%s/.docker/config.json" % home, 'w') as f:
         json.dump(docker_config, f, indent=4, sort_keys=True)
         print("Proxy activated for Docker")
+
 
 def deactivate():
     docker_config = {}
@@ -33,33 +39,15 @@ def deactivate():
         print("Proxy deactivated for Docker")
 
 
-def toggle():
-    docker_config = {}
-    with open("%s/.docker/config.json" % home) as f:
-        docker_config = json.load(f)
-        if "proxies" in docker_config:
-            del docker_config["proxies"]
-            print("Proxy deactivated for Docker")
-        else:
-            docker_config["proxies"] = PROXY_CONFIG
-            print("Proxy activated for Docker")
-
-    with open("%s/.docker/config.json" % home, 'w') as f:
-        json.dump(docker_config, f, indent=4, sort_keys=True)
-
-
 def main(argv):
 
-    if argv[0] in ["up", "on", "1"]:
+    if argv[0] in ["up", "on", "1", "activate", "enable"]:
         activate()
         sys.exit(0)
-    if argv[0] in ["down", "dn", "off", "0"]:
+    if argv[0] in ["down", "dn", "off", "0", "deactivate", "disable"]:
         deactivate()
-        sys.exit(0)
-    if argv[0] in ["tog", "toggle"]:
-        toggle()
         sys.exit(0)
 
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
