98362846d13c6cf914d8ffa38171e0438f9c6fbb
ftorto <dev.torto@gmail.com>
2018-06-20 11:47:14 +0200

    activation/deactivation of docker proxy config added
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- docker_conf_proxy.py
+++ docker_conf_proxy.py
@@ -1,5 +1,6 @@
-#!/bin/python3
+#!/usr/bin/python3
 
+import sys
 import json
 
 from os.path import expanduser
@@ -12,16 +13,53 @@ PROXY_CONFIG = {
     }
 }
 
-docker_config = {}
-with open("%s/.docker/config.json" % home) as f:
-    docker_config = json.load(f)
-    if "proxies" in docker_config:
-        print ("Disable proxy")
-        del docker_config["proxies"]
-    else:
-        print ("Enable proxy")
+def activate():
+    docker_config = {}
+    with open("%s/.docker/config.json" % home) as f:
+        docker_config = json.load(f)
         docker_config["proxies"] = PROXY_CONFIG
+    with open("%s/.docker/config.json" % home, 'w') as f:
+        json.dump(docker_config, f, indent=4, sort_keys=True)
+        print("Proxy activated for Docker")
 
-with open("%s/.docker/config.json" % home, 'w') as f:
-    json.dump(docker_config, f, indent=4, sort_keys=True)
-    print("Configuration written")
+def deactivate():
+    docker_config = {}
+    with open("%s/.docker/config.json" % home) as f:
+        docker_config = json.load(f)
+        if "proxies" in docker_config:
+            del docker_config["proxies"]
+    with open("%s/.docker/config.json" % home, 'w') as f:
+        json.dump(docker_config, f, indent=4, sort_keys=True)
+        print("Proxy deactivated for Docker")
+
+
+def toggle():
+    docker_config = {}
+    with open("%s/.docker/config.json" % home) as f:
+        docker_config = json.load(f)
+        if "proxies" in docker_config:
+            del docker_config["proxies"]
+            print("Proxy deactivated for Docker")
+        else:
+            docker_config["proxies"] = PROXY_CONFIG
+            print("Proxy activated for Docker")
+
+    with open("%s/.docker/config.json" % home, 'w') as f:
+        json.dump(docker_config, f, indent=4, sort_keys=True)
+
+
+def main(argv):
+
+    if argv[0] in ["up", "on", "1"]:
+        activate()
+        sys.exit(0)
+    if argv[0] in ["down", "dn", "off", "0"]:
+        deactivate()
+        sys.exit(0)
+    if argv[0] in ["tog", "toggle"]:
+        toggle()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
