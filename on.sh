1461bfb4ff1f477dd91e538d5c1bdc17890f9d45
ftorto <dev.torto@gmail.com>
2018-11-08 14:05:33 +0100

    socks wip

--- on.sh
+++ on.sh
@@ -4,13 +4,15 @@
 
 source ${HOME}/.byebyeproxy.conf
 test -z $PROXY_URL_HTTP && echo "PROXY_URL_HTTP not filled in ${HOME}/.byebyeproxy.conf" && exit 1
-test -z $PROXY_URL_HTTPS && echo "PROXY_URL_HTTPS not filled in ${HOME}/.byebyeproxy.conf" && exit 1
 
 if [ -z $(docker ps --filter="ancestor=ftorto/byebyeproxy" -q) ]
 then
   docker run -it --net=host --privileged -d \
     -e http_proxy=${PROXY_URL_HTTP} \
-    -e https_proxy=${PROXY_URL_HTTPS} \
+    -e https_proxy=${PROXY_URL_HTTPS:-${PROXY_URL_HTTP}} \
+    -e proxy_socks=${PROXY_SOCKS:-${PROXY_URL_HTTPS}} \
+    -e SOCKS_LOGIN=${SOCKS_LOGIN:-""} \
+    -e SOCKS_PASSWORD=${SOCKS_PASSWORD:-""} \
     ftorto/byebyeproxy:${1:-latest} > /dev/null 2>&1 && echo "byebyeproxy enabled"
 else
   echo "byebyeproxy already enabled"
